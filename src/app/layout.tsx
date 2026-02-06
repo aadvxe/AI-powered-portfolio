120c1115c8de9b0c89854351cee55636cdc809a6
aadvxe <danielranggahardiyanto@gmail.com>
2026-02-07 02:12:53 +0700

    updated open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,9 @@ export const metadata: Metadata = {
   description: "aadvxe's Personal Portfolio",
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://aadvxe.vercel.app"),
   icons: {
-    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>",
+    icon: "/apple-tech-emoji.png",
+    shortcut: "/apple-tech-emoji.png",
+    apple: "/apple-tech-emoji.png",
   },
   openGraph: {
     title: "aadvxe's Portfolio",
--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -11,6 +11,10 @@ export const size = {
 export const contentType = 'image/png'
 
 export default async function Image() {
+  const emojiData = await fetch(new URL('https://raw.githubusercontent.com/iamcal/emoji-data/master/img-apple-160/1f468-200d-1f4bb.png')).then(
+    (res) => res.arrayBuffer()
+  )
+
   return new ImageResponse(
     (
       <div
@@ -22,24 +26,24 @@ export default async function Image() {
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
-          fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+          fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
         }}
       >
         <div
             style={{
-                fontSize: 160,
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
                 marginBottom: 20,
             }}
         >
-            👨‍💻
+             {/* @ts-ignore */}
+             <img src={emojiData} width="160" height="160" />
         </div>
         <div
           style={{
             fontSize: 64,
-            fontWeight: 'bold',
+            fontWeight: 800,
             color: '#000',
             letterSpacing: '-2px',
           }}
